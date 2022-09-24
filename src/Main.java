e6881658a0bd282441c71b040884a11e74ef5d9d
bilal karahan <bilalkarahan309@gmail.com>
2022-09-24 19:43:47 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int sayac,sayi,min=0,max=0;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Kac sayi gireceksiniz : ");
+        sayac = input.nextInt();
+
+        for(int i=1; i<=sayac;i++){
+            System.out.print(i + ". Sayiyi giriniz : "  );
+            sayi = input.nextInt();
+
+            if (i==1){
+                max=sayi;
+                min=sayi;
+            }else{
+                if (sayi>max){
+                    max=sayi;
+                } else if (sayi<min) {
+                    min=sayi;
+                }
+            }
+        }
+        System.out.println("max = " + max);
+        System.out.println("min = " + min);
     }
 }
